06be0d065ebf7beab86feb01231c8a349b304656
JulianaAmoasei <late.parrot@gmail.com>
2022-08-18 03:17:57 -0300

    video: Promessas

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ function trataErro(erro) {
 
 function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
-  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-    if (erro) {
-      trataErro(erro);
-    }
-    console.log(chalk.green(texto));
-  })
+  fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch(trataErro)
 }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//     if (erro) {
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(texto));
+//   })
+// }
 
 pegaArquivo('./arquivos/');
