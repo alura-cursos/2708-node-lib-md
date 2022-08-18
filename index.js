010ae0d67c9fd4722b30cb4fc6bb4854473106b3
JulianaAmoasei <late.parrot@gmail.com>
2022-08-18 03:42:44 -0300

    video: async/await

--- index.js
+++ index.js
@@ -6,22 +6,28 @@ function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-  const encoding = 'utf-8';
-  fs.promises
-    .readFile(caminhoDoArquivo, encoding)
-    .then((texto) => console.log(chalk.green(texto)))
-    .catch(trataErro)
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto));
+  } catch (erro) {
+    trataErro(erro)
+  }
 }
+
+
+// promises com then()
 
 // function pegaArquivo(caminhoDoArquivo) {
 //   const encoding = 'utf-8';
-//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//     if (erro) {
-//       trataErro(erro);
-//     }
-//     console.log(chalk.green(texto));
-//   })
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch(trataErro)
 // }
 
+pegaArquivo('./arquivos/texto.md');
 pegaArquivo('./arquivos/');
