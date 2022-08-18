3abd631afdab313765e1f3ef175c0994a2ac312e
JulianaAmoasei <late.parrot@gmail.com>
2022-08-18 02:02:20 -0300

    video: tratamento de erros

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
 function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
-  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+    if (erro) {
+      trataErro(erro);
+    }
     console.log(chalk.green(texto));
   })
 }
 
-pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
