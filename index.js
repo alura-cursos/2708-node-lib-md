8f18ed17af8b85646940aba694ec99e2d36d70b8
JulianaAmoasei <late.parrot@gmail.com>
2022-08-18 00:26:43 -0300

    video: instalando dependencias

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+
+console.log(chalk.blue('olá mundo'));
+
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/');
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/');
