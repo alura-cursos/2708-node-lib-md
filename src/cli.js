2eb2ab32df161b900dc77c0601b25e60431c15fc
JulianaAmoasei <late.parrot@gmail.com>
2022-08-19 17:48:34 -0300

    video: organizando entradas

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,11 @@
+import chalk from 'chalk';
 import pegaArquivo from './index.js';
 
 const caminho = process.argv;
 
-pegaArquivo(caminho[2]);
+async function processaTexto(caminho) {
+  const resultado = await pegaArquivo(caminho[2]);
+  console.log(chalk.yellow('lista de links'), resultado);
+}
+
+processaTexto(caminho);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function extraiLinks(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
   const capturas = [...texto.matchAll(regex)];
   const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-  return resultados;
+  return resultados.length !== 0 ? resultados : 'não há links no arquivo';
 }
 
 function trataErro(erro) {
@@ -19,7 +19,7 @@ async function pegaArquivo(caminhoDoArquivo) {
   try {
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(extraiLinks(texto));
+    return extraiLinks(texto);
   } catch (erro) {
     trataErro(erro)
   }
