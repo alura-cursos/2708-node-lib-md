a947ae95babd9dd09f22b94fa1d02d8ea3370985
JulianaAmoasei <late.parrot@gmail.com>
2022-08-19 17:28:14 -0300

    video: executando comandos

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import pegaArquivo from './index.js';
+
+const caminho = process.argv;
+
+pegaArquivo(caminho[2]);
--- src/index.js
+++ src/index.js
@@ -25,4 +25,4 @@ async function pegaArquivo(caminhoDoArquivo) {
   }
 }
 
-pegaArquivo('./arquivos/texto.md');
+export default pegaArquivo;
