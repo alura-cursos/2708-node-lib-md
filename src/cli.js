7c85311c6d892e19c6d02467eb44ddd88d15972a
JulianaAmoasei <late.parrot@gmail.com>
2022-08-19 21:06:26 -0300

    video: processando diretorios

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,27 @@
 import chalk from 'chalk';
+import fs from 'fs';
 import pegaArquivo from './index.js';
 
 const caminho = process.argv;
 
-async function processaTexto(caminho) {
-  const resultado = await pegaArquivo(caminho[2]);
+function imprimeLista(resultado) {
   console.log(chalk.yellow('lista de links'), resultado);
+}
+
+
+async function processaTexto(argumentos) {
+  const caminho = argumentos[2];
+
+  if (fs.lstatSync(caminho).isFile()) {
+    const resultado = await pegaArquivo(argumentos[2]);
+    imprimeLista(resultado);
+  } else if (fs.lstatSync(caminho).isDirectory()) {
+    const arquivos = await fs.promises.readdir(caminho)
+    arquivos.forEach(async (nomeDeArquivo) => {
+      const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+      imprimeLista(lista)
+    })
+  }
 }
 
 processaTexto(caminho);
