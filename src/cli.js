f20bc217704e9f06dc1c809f28db730ea2a6d807
JulianaAmoasei <late.parrot@gmail.com>
2022-08-22 16:16:19 -0300

    video: tratando novos erros

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,25 @@ import pegaArquivo from './index.js';
 
 const caminho = process.argv;
 
-function imprimeLista(resultado) {
-  console.log(chalk.yellow('lista de links'), resultado);
+function imprimeLista(resultado, identificador = '') {
+  console.log(
+    chalk.yellow('lista de links'),
+    chalk.black.bgGreen(identificador),
+    resultado);
 }
 
 
 async function processaTexto(argumentos) {
   const caminho = argumentos[2];
+
+  try {
+    fs.lstatSync(caminho);
+  } catch (erro) {
+    if (erro.code === 'ENOENT') {
+      console.log('arquivo ou diretório não existe');
+      return;
+    }
+  }
 
   if (fs.lstatSync(caminho).isFile()) {
     const resultado = await pegaArquivo(argumentos[2]);
@@ -19,7 +31,7 @@ async function processaTexto(argumentos) {
     const arquivos = await fs.promises.readdir(caminho)
     arquivos.forEach(async (nomeDeArquivo) => {
       const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-      imprimeLista(lista)
+      imprimeLista(lista, nomeDeArquivo)
     })
   }
 }
