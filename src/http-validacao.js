e850cc1edb4eb64f7d9d2227ba5264075eb3da09
JulianaAmoasei <late.parrot@gmail.com>
2022-08-22 23:43:01 -0300

    video: Gerando uma lista de links

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,3 +1,7 @@
+function extraiLinks (arrLinks) {
+  return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
+}
+
 export default function listaValidada (listaDeLinks) {
-  return 'entrou na função';
+  return extraiLinks(listaDeLinks);
 }
