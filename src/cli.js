c48ff29448b68c40acdd55b1fea2ce68bb0b1ed8
JulianaAmoasei <late.parrot@gmail.com>
2022-08-22 23:24:02 -0300

    video: adicionando opções

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,28 @@
 import chalk from 'chalk';
 import fs from 'fs';
 import pegaArquivo from './index.js';
+import listaValidada from './http-validacao.js';
 
 const caminho = process.argv;
 
-function imprimeLista(resultado, identificador = '') {
-  console.log(
-    chalk.yellow('lista de links'),
-    chalk.black.bgGreen(identificador),
-    resultado);
+function imprimeLista(valida, resultado, identificador = '') {
+  if (valida) {
+    console.log(
+      chalk.yellow('lista validada'),
+      chalk.black.bgGreen(identificador),
+      listaValidada(resultado));    
+  } else {
+    console.log(
+      chalk.yellow('lista de links'),
+      chalk.black.bgGreen(identificador),
+      resultado);
+  }
 }
 
 
 async function processaTexto(argumentos) {
   const caminho = argumentos[2];
+  const valida = argumentos[3] === '--valida';
 
   try {
     fs.lstatSync(caminho);
@@ -26,12 +35,12 @@ async function processaTexto(argumentos) {
 
   if (fs.lstatSync(caminho).isFile()) {
     const resultado = await pegaArquivo(argumentos[2]);
-    imprimeLista(resultado);
+    imprimeLista(valida, resultado);
   } else if (fs.lstatSync(caminho).isDirectory()) {
     const arquivos = await fs.promises.readdir(caminho)
     arquivos.forEach(async (nomeDeArquivo) => {
       const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-      imprimeLista(lista, nomeDeArquivo)
+      imprimeLista(valida, lista, nomeDeArquivo)
     })
   }
 }
