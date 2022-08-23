10d781f100dd81d213b1d53073ef42878533e7f9
JulianaAmoasei <late.parrot@gmail.com>
2022-08-23 02:22:59 -0300

    video: montando o objeto

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
 function extraiLinks (arrLinks) {
   return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
@@ -6,17 +8,31 @@ async function checaStatus (listaURLs) {
   const arrStatus = await Promise
   .all(
     listaURLs.map(async (url) => {
-      const response = await fetch(url)
-      return response.status;
+      try {
+        const response = await fetch(url)
+        return response.status;
+      } catch (erro) {
+        return manejaErros(erro);
+      }
     })
   )
   return arrStatus;
 }
 
+function manejaErros (erro) {
+  if (erro.cause.code === 'ENOTFOUND') {
+    return 'link não encontrado';
+  } else {
+    return 'ocorreu algum erro';
+  }
+}
+
 export default async function listaValidada (listaDeLinks) {
   const links = extraiLinks(listaDeLinks);
   const status = await checaStatus(links);
-  return status;
-}
 
-//[gatinho salsicha](http://gatinhosalsicha.com.br/)
+  return listaDeLinks.map((objeto, indice) => ({
+    ...objeto,
+    status: status[indice]
+  }))
+}
