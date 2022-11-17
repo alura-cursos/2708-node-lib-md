06062c82eb4eebe6f0cbd9ea6a74d0d93af482e5
GusTeixeira1 <gustavo.teixeira@lucree.com.br>
2022-11-17 20:35:21 -0300

    Começando com FileSystem e com tratamento de erros

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+import fs from 'fs';
+
+function trataErro(erro){
+    throw new Error(erro);
+}
+function pegaArquivo(caminhoDoArquivo) {
+    const enconding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, enconding, (erro, texto) => {
+        if(erro){
+            trataErro(chalk.red(erro.code, 'não há arquivo no diretório'));
+        }
+        console.log(chalk.green(texto));
+    } )
+}
+pegaArquivo('./arquivos/texto.md'); 
