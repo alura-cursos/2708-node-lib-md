e9b0cc38c92a1da4d7048d5b7b0bde8698e460fc
GusTeixeira1 <gustavo.teixeira@lucree.com.br>
2022-11-17 20:28:44 -0300

    Tentativa de armazenar erros em um array e compara-los com o erro dado

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+import fs from 'fs';
+
+function trataErro(erro){
+    throw new Error(erro);
+}
+function pegaArquivo(caminhoDoArquivo) {
+    const arrayErros = [['EACCES', 'Acesso ao arquivo negado, por favor verifique suas permissões'], 
+    ['EEXIST', 'O arquivo exite porém referência outro arquivo não existente'],
+    ['EISDIR', 'A operação esperava um arquivo porém encontrou um diretório'],
+    ['ENOENT', 'O arquivo ou diretório não existem'],
+    ['ENOTDIR', 'Esperava um diretório, porém não encontrou']]
+    
+    const enconding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, enconding, (erro, texto) => {
+        
+        if(erro){
+            for(var i = 0; i < 5; i++){
+                if(erro.code == arrayErros[i, 0]){
+                    trataErro(chalk.red(erro.code, arrayErros[1, i]));
+                }
+        }
+    }
+            console.log(chalk.green(texto));
+    } )
+}
+pegaArquivo('./arquivos/');
