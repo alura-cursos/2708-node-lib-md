c3386f9903fe96ff3174b7dcba351ed170ceaae5
GusTeixeira1 <gustavo.teixeira@lucree.com.br>
2022-11-18 17:43:10 -0300

    Codigo Assincrono com Promises

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+const arrayErrors = {
+    'EACCES':'Acesso ao arquivo negado, por favor verifique suas permissões', 
+    'EEXIST':'O arquivo exite porém referência outro arquivo não existente',
+    'EISDIR':'A operação esperava um arquivo porém encontrou um diretório',
+    'ENOENT':'O arquivo ou diretório não existem',
+    'ENOTDIR':'Esperava um diretório, porém não encontrou'
+}
+
 function trataErro(erro){
-    throw new Error(erro);
+    throw new Error(chalk.red(erro.code, arrayErrors[erro.code]));
 }
 function pegaArquivo(caminhoDoArquivo) {
     const enconding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, enconding, (erro, texto) => {
-        const arrayErrors = {
-            'EACCES':'Acesso ao arquivo negado, por favor verifique suas permissões', 
-            'EEXIST':'O arquivo exite porém referência outro arquivo não existente',
-            'EISDIR':'A operação esperava um arquivo porém encontrou um diretório',
-            'ENOENT':'O arquivo ou diretório não existem',
-            'ENOTDIR':'Esperava um diretório, porém não encontrou'
-        }
-
-        if(erro){
-                    trataErro(chalk.red(erro.code, arrayErrors[erro.code])); 
-    }
-            console.log(chalk.green(texto));
-    } )
+    fs.promises.readFile(caminhoDoArquivo, enconding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch(trataErro)
 }
 pegaArquivo('./arquivos/texto.md');
