9a64a0d5fe75f7f53bf0e41641e73c599c155a01
GusTeixeira1 <gusteixeira3004@gmail.com>
2022-12-21 18:22:15 -0300

    adicionando coisas

--- src/cli.js
+++ src/cli.js
@@ -3,21 +3,26 @@ import fs from 'fs';
 import chalk from 'chalk';
 const argumentos = process.argv;
 
-function imprimeLista(resultado,identificador = ''){
-    console.log(chalk.yellow('lista de links'), chalk.black.bgGreen(identificador), resultado)
+function imprimeLista(valida,resultado,identificador = ''){
+    if(valida){
+    console.log(chalk.yellow('lista validada'), chalk.black.bgGreen(identificador), listaValidada(resultado));
+    } else{
+    console.log(chalk.yellow('lista de links'), chalk.black.bgGreen(identificador), resultado);
+    }
 }
 async function processaTexto(argumentos){
-    const caminho = argumentos[2]
-
+    const caminho = argumentos[2];
+    const valida = argumentos[3] === '--valida';
+    console.log(valida)
     if(fs.lstatSync(caminho).isFile()){
         const resultado = await pegaArquivo(caminho)
-        imprimeLista(resultado);
+        imprimeLista(valida, resultado);
 
     }else if(fs.lstatSync(caminho).isDirectory()){
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach(async(nomeDeArquivo) =>{
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-            imprimeLista(lista, nomeDeArquivo)
+            imprimeLista(valida, lista, nomeDeArquivo)
         })
     }
 }
