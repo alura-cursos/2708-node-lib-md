a34175bdf14ae35ec9d982f116be5445eb8022db
João Donato <joaod.morais@gmail.com>
2023-01-30 18:27:16 -0300

    aula 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+
+import fs from 'fs';
+
+function pegaArquivo (caminhoDoArquivo){
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+        console.log(chalk.greenBright(texto));
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
