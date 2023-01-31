f05d46cb9dfa06a31e93fce099dbf9b28f38d434
João Donato <joaod.morais@gmail.com>
2023-01-31 08:37:04 -0300

    add funcao trataErro

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ import chalk from 'chalk';
 
 import fs from 'fs';
 
+function trataErro (erro) {
+    throw new Error(erro);
+}
+
 function pegaArquivo (caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
-        console.log(chalk.greenBright(texto));
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if(erro){
+            trataErro(erro);
+        }
+
+        console.log(chalk.bgGray(texto));
     })
 }
 
