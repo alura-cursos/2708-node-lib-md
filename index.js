a36e26580b4ee95bdc32611c069af318e6af8b97
caio magalhaes <caio.magalhaes02@hotmail.com>
2023-04-20 21:44:15 -0300

    criando meu diretorio

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('olá mundo');
+import fs from "fs";
+
+import chalk from "chalk";
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, "não ah arquivo no diretorio"));
+}
+
+console.log(chalk.blue("ola mundo "));
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = "utf-8";
+
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//     if (erro) {
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(texto));
+//   });
+// }
+
+function pegaArquivo(caminhoDoArquivo) {
+  const encoding = "utf-8";
+  fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.green(texto)));
+}
+
+pegaArquivo("./arquivos/texto.md");
